ba788122e2427a1871d3df4426d70394b7ef7a0f
carlosariasf <carlosarias@icreativa.com.co>
2020-11-20 13:30:51 -0500

    [add] current_user check at endpoint

--- app/controllers/devise/phone_verifications_controller.rb
+++ app/controllers/devise/phone_verifications_controller.rb
@@ -16,22 +16,23 @@ class Devise::PhoneVerificationsController < DeviseController
     current_user.generate_verification_code_and_send_sms
     # render nothing: true
     respond_to do |format|
-      msg = { :status => "ok", :message => "SMS sent!" }
+      msg = { :status => 'ok', :message => 'SMS sent!' }
       format.json  { render :json => msg } # don't do msg.to_json
     end
   end
 
   # GET or POST /resource/phone_verification/verify_code
   def verify_code
+    render json: '{error: invalid token}', status: :unprocessable_entity unless current_user
     verify_success = current_user.verify_phone_number_with_code_entered(params[:code_entered])
     # render nothing: true
     respond_to do |format|
       if verify_success
-        message_response = "verification successful"
+        message_response = 'verification successful'
       else
-        message_response = "verification fail"
+        message_response = 'verification fail'
       end
-      msg = { :status => "ok", :message => message_response }
+      msg = { :status => 'ok', :message => message_response }
       format.json  { render :json => msg } # don't do msg.to_json
     end
   end
